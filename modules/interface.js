b006e2603c0b20f234009823ae4d81f84d51dbd4
Daniel <danielfspencer1@gmail.com>
2020-05-26 20:59:23 +0100

    Remove 'tiny-worker' module in favour of 'vm' module
    Also use template literals to clean up code

---
+++ modules/interface.js
@@ -0,0 +1,80 @@
+const log = require('../modules/log.js')
+
+const vm = require('vm')
+const fs = require('fs')
+const path = require('path')
+const { performance } = require('perf_hooks')
+
+const COMPILER_ENGINE = fs.readFileSync(path.join(__dirname, '../compiler/engine.js'))
+const COMPILER_LIBRARIES = fs.readFileSync(path.join(__dirname, '../compiler/libraries.js'))
+const ASSEMBLER_ENGINE = fs.readFileSync(path.join(__dirname, '../assembler/engine.js'))
+
+function create_context(files, log_prefix) {
+  // create fake WebWorker environment
+  const context = {
+    importScripts: () => {},
+    performance: performance,
+    postMessage: (data) => log.handle_worker_log(log_prefix, data),
+  }
+
+  // instantiate the context with the given global vars
+  vm.createContext(context)
+  // create dummy message handler to prevent errors when trying to register handlers
+  vm.runInContext('let onmessage', context)
+
+  // load & evaluate each file in the context
+  for (const [source, name] of files) {
+    vm.runInContext(source, context, {filename: name})
+  }
+
+  // set debug logging status
+  vm.runInContext(`debug = ${exports.debug}`, context)
+  return context
+}
+
+exports.debug = false
+
+exports.compile = (input) => {
+  const LOG_PREFIX = ' cmp'
+  let context = create_context([
+    [COMPILER_LIBRARIES, 'libraries.js'],
+    [COMPILER_ENGINE, 'engine.js']
+  ], LOG_PREFIX)
+
+  return new Promise((resolve, reject) => {
+    let result = null
+    try {
+      result = context.compile_wrapped(input, false)
+    } catch (e) {
+      log.error(`${LOG_PREFIX}: [Internal compiler error]\n${e.stack}`)
+    }
+
+    if (result === null) {
+      reject()
+    } else {
+      resolve([result, context.get_ast()])
+    }
+  })
+}
+
+exports.assemble = (input) => {
+  const LOG_PREFIX = ' asm'
+  let context = create_context([
+    [ASSEMBLER_ENGINE, 'engine.js'],
+  ], LOG_PREFIX)
+
+  return new Promise((resolve, reject) => {
+    let result = null
+    try {
+      result = context.assemble_wrapped(input)
+    } catch (e) {
+      log.error(`${LOG_PREFIX}: [Internal assembler error]\n${e.stack}`)
+    }
+
+    if (result === null) {
+      reject()
+    } else {
+      resolve(result)
+    }
+  })
+}
--- modules/log.js
+++ modules/log.js
@@ -3,27 +3,42 @@ const colors = require('colors/safe')
 exports.quiet = false
 
 exports.error = (msg) => {
-  console.error(colors.red('error') + ' ' + toString(msg))
+  console.error(`${colors.red('error')} ${toString(msg)}`)
 }
 
 exports.warn = (msg) => {
-  if (exports.quiet) {return}
-  console.warn(' ' + colors.yellow('warn') + ' ' + toString(msg))
+  if (exports.quiet) { return }
+  console.warn(` ${colors.yellow('warn')} ${toString(msg)}`)
 }
 
 exports.info = (msg) => {
-  if (exports.quiet) {return}
-  console.info(' ' + colors.blue('info') + ' ' + toString(msg))
+  if (exports.quiet) { return }
+  console.info(` ${colors.blue('info')} ${toString(msg)}`)
 }
 
 exports.debug = (msg) => {
-  if (exports.quiet) {return}
-  console.info(colors.grey('debug') + ' ' + toString(msg))
+  if (exports.quiet) { return }
+  console.info(colors.dim(`${colors.grey('debug')} ${toString(msg)}`))
+}
+
+exports.handle_worker_log = (prefix, data) => {
+  let [type, level, value] = data
+  let msg = `${prefix}: ${value}`
+  switch (level) {
+    case 'debug': exports.debug(msg)
+    break
+    case 'info': exports.info(msg)
+    break
+    case 'warn': exports.warn(msg)
+    break
+    case 'error': exports.error(msg)
+    break
+  }
 }
 
 function toString(obj) {
-  if (typeof message === 'object') {
-    return padNewlines(JSON.stringify(obj))
+  if (typeof obj === 'object') {
+    return JSON.stringify(obj, null, 2)
   } else {
     return padNewlines(obj)
   }
